a4f24a4bf6fed9b418cb15e6edfd7d70eccef2b5
muffindud <ccatlabuga2002@gmail.com>
2023-10-29 10:53:09 +0200

    task 6

---
+++ lab1/lab1_6.asm
@@ -0,0 +1,23 @@
+; lab1_6.asm
+; Display string
+org 0x7C00
+
+jmp main
+
+section .text
+    msg db "Hello", 0
+
+main:
+    mov sp, 0x7C00
+    
+    mov ax, 1300h
+    mov bl, 0x02
+    
+    mov dh, 0
+    mov dl, 0
+    
+    mov bp, msg
+    mov cx, 6
+
+    int 10H
+    
