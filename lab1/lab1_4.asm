c81cf203bc531e6ba1c33f86d15ee1ea0a4b68ff
muffindud <ccatlabuga2002@gmail.com>
2023-10-29 10:47:32 +0200

    task 4

---
+++ lab1/lab1_4.asm
@@ -0,0 +1,21 @@
+; lab1_4.asm
+; Display character + attribute
+org 0x7C00
+
+jmp main
+
+section .text
+    msg db 'H', 0x09, 'e', 0x0E, 'l', 0x0B, 'l', 0x0C, 'o', 0x0D
+
+main:
+    mov sp, 0x7C00
+
+    mov ax, 1302H
+
+    mov dh, 0
+    mov dl, 0
+
+    mov bp, msg
+    mov cx, 6
+
+    int 10H
